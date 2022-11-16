01a95e3f57c8700ce5da38465d39cce03ec2f816
Zeki Berk Tonguç <102914036+BerkTonguc@users.noreply.github.com>
2022-11-16 21:14:12 +0300

    Add files via upload

---
+++ sartBloklariylaAlgoritmaMantiginiAnlamak.py
@@ -0,0 +1,20 @@
+dolarDun = 7.85
+dolarBugun=7.85
+
+if dolarDun>dolarBugun:
+    print("Azalış Oku")
+    
+
+elif dolarDun<dolarBugun:
+    print("Artış Oku")
+
+else:
+    print("Eşittir Oku")
+
+print("Bitti")
+
+#if dolarDun<dolarBugun:
+    #print("Artış Oku")
+
+#if dolarDun==dolarBugun:
+    #print("Eşittir Oku")
